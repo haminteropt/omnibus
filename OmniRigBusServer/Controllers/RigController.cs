b0a981f5f72626abe83aaf379bce4a25a7753d63
DELENN\Darryl <darryl@wagoner.me>
2018-04-26 07:13:45 -0500

    Simple get for rig

---
+++ OmniRigBusServer/Controllers/RigController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OmniRigBusServer.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OmniRigBusServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class RigController : Controller
+    {
+        public RigController()
+        {
+
+        }
+        [HttpGet]
+        public IEnumerable<RadioTuningInfo> Get()
+        {
+            RadioTuningInfo[] rti  = { new RadioTuningInfo()};
+            return rti;
+        }
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ OmniRigBusServer/Models/Frequency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmniRigBusServer.Models
+{
+    public class RadioTuningInfo
+    {
+        public string mode;
+        public decimal frequency;
+        public RadioTuningInfo()
+        {
+            mode = "USB";
+            frequency = 14.213M;
+        }
+       
+    }
+}
