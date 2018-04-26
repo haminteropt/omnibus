4b93b260ca90705da43f4eccfa8d72804039d6d9
DELENN\Darryl <darryl@wagoner.me>
2018-04-25 19:58:36 -0500

    Start of project

--- OmniRigBusServer/Program.cs
+++ OmniRigBusServer/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication1
+namespace OmniRigBusServer
 {
     public class Program
     {
