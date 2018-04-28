348f47ebae83aba645baf8b4579f108d8dd2c42f
DELENN\Darryl <darryl@wagoner.me>
2018-04-27 21:41:34 -0500

    Start of omniRigBus

---
+++ OmniRigBus/Controller/RigController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OmniRigBus.Controller
+{
+    public class RigController : ApiController
+    {
+        // GET api/rig 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+
+        // GET api/rig/5 
+        public string Get(int id)
+        {
+            return "Hello, World!";
+        }
+
+        // POST api/demo 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/demo/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/demo/5 
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
