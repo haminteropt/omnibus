098bb9c3ceaaad21cabdaf2252e4e2475ce5fc7a
DELENN\Darryl <darryl@wagoner.me>
2018-05-10 21:08:07 -0500

    got most of the objects

--- OmniRigBus/ORig.cs
+++ OmniRigBus/ORig.cs
@@ -1,9 +1,11 @@
-﻿using OmniRig;
+﻿using Newtonsoft.Json;
+using OmniRig;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace OmniRigBus
 {
@@ -22,9 +24,30 @@ namespace OmniRigBus
         }
         private void ParamsChangeEvent(int RigNumber, int Params)
         {
-            //if (RigNumber == null) return;
+            if (RigNumber != 1) return;
+
+            var rigState = RigState.Instance;
+
+            rigState.Freq = Rig.Freq;
+            rigState.FreqA = Rig.FreqA;
+            rigState.FreqB = Rig.FreqB;
+
+            rigState.Mode = Rig.Mode;
+            rigState.Pitch = Rig.Pitch;
+            rigState.RigType = Rig.RigType;
+
+            rigState.Rit = Rig.Rit;
+            rigState.RitOffset = Rig.RitOffset;
+            rigState.Split = Rig.Split;
+            rigState.Status = Rig.Status;
+            rigState.Tx = Rig.Tx;
+            rigState.Vfo = Rig.Vfo;
+            rigState.Xit = Rig.Xit;
+
             Console.WriteLine( Rig.FreqA.ToString());
             Console.WriteLine(Rig.Mode.ToString());
+            var json = JsonConvert.SerializeObject(rigState);
+            Console.Write(json);
 
         }
         public static ORig Instance
--- OmniRigBus/RestRig/RigState.cs
+++ OmniRigBus/RestRig/RigState.cs
@@ -24,12 +24,16 @@ namespace OmniRigBus
         public int Freq { get; set; }
         public int FreqA { get; set; }
         public int FreqB { get; set; }
-        public int Mode { get; set; }
+        public RigParamX Mode { get; set; }
         public int Pitch { get; set; }
-        public int Rit { get; set; }
+        public string RigType { get; set; }
+        public RigParamX Rit { get; set; }
         public int RitOffset { get; set; }
-        public int RigType { get; set; }
-        public int Status { get; set; }
+        public RigStatusX Status { get; set; }
         public string StatusStr { get; set; }
+        public RigParamX Split { get; set; }
+        public RigParamX Tx { get; set; }
+        public RigParamX Vfo { get; set; }
+        public RigParamX Xit { get; set; }
     }
 }
