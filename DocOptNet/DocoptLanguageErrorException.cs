bc477500c7fc3f8a65bf4bf75dc77c8838fc40ad
DELENN\Darryl <darryl@wagoner.me>
2018-05-21 20:06:28 -0500

    DocOptNet

---
+++ DocOptNet/DocoptLanguageErrorException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DocoptNet
+{
+#if NET40
+    [Serializable]
+#endif
+    public class DocoptLanguageErrorException : DocoptBaseException
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public DocoptLanguageErrorException()
+        {
+        }
+        public DocoptLanguageErrorException(string message) : base(message)
+        {
+        }
+        public DocoptLanguageErrorException(string message, Exception inner) : base(message, inner)
+        {
+        }
+#if NET40
+        protected DocoptLanguageErrorException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+#endif
+    }
+}
--- OmniRigBus/Program.cs
+++ OmniRigBus/Program.cs
@@ -4,13 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DocoptNet;
 
 namespace OmniRigBus
 {
     class Program
     {
+        private const string usage = @"Naval Fate.
+
+    Usage:
+      OmniRigBus.exe ship new <name>...
+      OmniRigBus.exe ship <name> move <x> <y> [--speed=<kn>]
+      OmniRigBus.exe ship shoot <x> <y>
+      OmniRigBus.exe mine (set|remove) <x> <y> [--moored | --drifting]
+      OmniRigBus.exe (-h | --help)
+      OmniRigBus.exe --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+      --speed=<kn>  Speed in knots [default: 10].
+      --moored      Moored (anchored) mine.
+      --drifting    Drifting mine.
+
+    ";
         static void Main(string[] args)
         {
+            var arguments = new Docopt().Apply(usage, args, version: "OmniRigBus 0.1", exit: true);
+            foreach (var argument in arguments)
+            {
+                Console.WriteLine("{0} = {1}", argument.Key, argument.Value);
+            }
+
             using (WebApp.Start<Startup>("http://localhost:7300"))
             {
                 Console.WriteLine("Web Server is running.");
