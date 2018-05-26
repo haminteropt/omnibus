5676de5f181677648967d9ff085f2e10cc298f81
B5\Darryl <darryl@wagoner.me>
2018-05-26 18:36:03 -0500

    add topshelf service support

--- OmniRigBus/Program.cs
+++ OmniRigBus/Program.cs
@@ -4,19 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.ServiceConfigurators;
 
 namespace OmniRigBus
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            {
+                var exitCode = HostFactory.Run(c =>
+                {
+                    c.Service<OmniRigService>(service =>
+                    {
+                        ServiceConfigurator<OmniRigService> s = service;
+                        s.ConstructUsing(() => new OmniRigService());
+                        s.WhenStarted(a => a.Start());
+                        s.WhenStopped(a => a.Stop());
+                    });
+                    c.SetServiceName("OmniRigHamBus");
+                    c.SetDisplayName("OmniRig HamBus server");
+                    c.SetDescription("Web server for OmniRig and hambus");
+                });
+                return (int)exitCode;
+            }
+            /*
             using (WebApp.Start<Startup>("http://localhost:7300"))
             {
                 Console.WriteLine("Web Server is running.");
                 Console.WriteLine("Press any key to quit.");
                 Console.ReadLine();
             }
+            */
         }
     }
 }
