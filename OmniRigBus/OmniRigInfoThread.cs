fb1ddaa31c6d219e4ac22ddfb5d3792ed8743766
DELENN\wa1go <darryl@hambus.org>
2018-07-19 20:17:49 -0500

    added timeout for udp read

--- OmniRigBus/OmniRigInfoThread.cs
+++ OmniRigBus/OmniRigInfoThread.cs
@@ -58,21 +58,35 @@ namespace OmniRigBus
             DirGreetingList dirList = DirGreetingList.Instance; 
             var ServerEp = new IPEndPoint(IPAddress.Any, 0);
             udpClient.EnableBroadcast = true;
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
             while (true)
             {
-                Byte[] senddata = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(rigBusDesc));
-                rigBusDesc.Time = DateTimeUtils.ConvertToUnixTime(DateTime.Now);
-
-                udpClient.Send(senddata, senddata.Length, new IPEndPoint(IPAddress.Broadcast, 7300));
-                Console.WriteLine("Sending Data");
-                var ServerResponseData = udpClient.Receive(ref ServerEp);
-                var ServerResponse = Encoding.ASCII.GetString(ServerResponseData);
-                Console.WriteLine("Recived {0} from {1} port {2}", ServerResponse, 
-                    ServerEp.Address.ToString(), ServerEp.Port);
-                var dirService = DirectoryBusGreeting.ParseCommand(ServerResponse);
-                DirGreetingList.Instance.Add(dirService);
-                Thread.Sleep(3000);
+                try
+                {
+                    ServerEp = SendUpdateResponse(ServerEp);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                }
             }
+        }
+
+        private IPEndPoint SendUpdateResponse(IPEndPoint ServerEp)
+        {
+            Byte[] senddata = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(rigBusDesc));
+            rigBusDesc.Time = DateTimeUtils.ConvertToUnixTime(DateTime.Now);
+
+            udpClient.Send(senddata, senddata.Length, new IPEndPoint(IPAddress.Broadcast, 7300));
+            Console.WriteLine("Sending Data");
+            var ServerResponseData = udpClient.Receive(ref ServerEp);
+            var ServerResponse = Encoding.ASCII.GetString(ServerResponseData);
+            Console.WriteLine("Recived {0} from {1} port {2}", ServerResponse,
+                ServerEp.Address.ToString(), ServerEp.Port);
+            var dirService = DirectoryBusGreeting.ParseCommand(ServerResponse);
+            DirGreetingList.Instance.Add(dirService);
+            Thread.Sleep(3000);
+            return ServerEp;
         }
     }
 }
